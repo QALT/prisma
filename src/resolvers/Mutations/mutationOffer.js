2dc716159a67be02250eee830c3fabe996a00434
Quentin Hermiteau <quentinhermiteau@gmail.com>
2021-01-26 18:42:29 +0100

    fix delete offer (#51)
    * fix delete offer
    
    * fix lint

--- src/resolvers/Mutations/mutationOffer.js
+++ src/resolvers/Mutations/mutationOffer.js
@@ -9,6 +9,12 @@ async function updateOffer (parent, args, ctx, info) {
 }
 
 async function deleteOffer (parent, args, ctx, info) {
+    const applications = await ctx.prisma.query.applications({ where: { offer: { id: args.where.id } } }, "{ id offer { id } }");
+
+    await applications.forEach(async application => {
+        await ctx.prisma.mutation.deleteApplication({ where: { id: application.id } }, "{ id }");
+    });
+
     return forwardTo("prisma")(parent, args, ctx, info);
 }
 
